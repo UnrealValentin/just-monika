962bca157aafd46e7c62bc5b54ef59b499e7ced5
UnrealValentin <wolfistgeil@gmail.com>
2020-05-04 19:34:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -155,7 +155,8 @@ client.on("message", async message => {
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
     
     // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    //{limit: deleteCount}
+    const fetched = await message.channel.messages.fetch({limit: deleteCount});
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
   }
